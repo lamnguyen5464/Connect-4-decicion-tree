640e21a372a6dc9af58e961dd057119c31f9c181
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-12-12 23:39:50 +0700

    feat: add max_depth

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
-from sklearn import tree
+from sklearn import tree, metrics
 import numpy
 import matplotlib.pyplot as plt
 import graphviz
 from models.FourConnectState import FourConnectState
-from utils.fileUtils  import getDatasetFile, getAllLineFromDataset
+from utils.fileUtils  import getAllLineFromDataset
 
 print('start...')
 
 all_data = getAllLineFromDataset()
 
-training_set = all_data[ : 30]
-testing_set = all_data[-10: -1]
+training_set = all_data[ : 800]
+testing_set = all_data[-200: -1]
 
 print("Read file done")
 
@@ -23,31 +23,40 @@ testing_set_states = list(map(lambda str: FourConnectState(str=str), testing_set
 X = list(map(lambda state: state.state, training_set_states))
 Y = list(map(lambda state: state.result, training_set_states))
 
-decision_tree = tree.DecisionTreeClassifier().fit(X, Y)
+max_depth = 10
+decision_tree = tree.DecisionTreeClassifier(max_depth=max_depth).fit(X, Y)
+print('max_depth', decision_tree.tree_.max_depth)
 
-res = decision_tree.predict(
+res_pred = decision_tree.predict(
 	list(map(lambda state: state.state, testing_set_states))	
 )
-
-ans = (list(map(lambda state: state.result, testing_set_states)))
+res_truth = (list(map(lambda state: state.result, testing_set_states)))
 
 cnt = 0
-for i in range(0, len(res)):
-	if res[i] == ans[i]:
+for i in range(0, len(res_pred)):
+	if res_pred[i] == res_truth[i]:
 		cnt = cnt + 1
 	
-print(cnt / len(res))
+print('accuracy', cnt / len(res_pred))
+
+confusion_matric = metrics.confusion_matrix(res_truth, res_pred, labels=['win', 'loss', 'draw'])
+print(confusion_matric)
+
+
+
+
+
 
 labels = ['A1', ' A2', ' A3', ' A4', ' A5', ' A6', ' A7', ' A8', ' A9', ' A10', ' A11', ' A12', ' A13', ' A14', ' A15', ' A16', ' A17', ' A18', ' A19', ' A20', ' A21', ' A22', ' A23', ' A24', ' A25', ' A26', ' A27', ' A28', ' A29', ' A30', ' A31', ' A32', ' A33', ' A34', ' A35', ' A36', ' A37', ' A38', ' A39', ' A40', ' A41', ' A42']
 
 
 dot_data = tree.export_graphviz(
-	decision_tree=decision_tree,
     filled=True,
 	rounded=True,
-    special_characters=True,
-	feature_names=labels
+	max_depth=max_depth,
+	feature_names=labels,
+	decision_tree=decision_tree
 ) 
 
 graph = graphviz.Source(dot_data) 
-graph.render(filename="test1",format='png',directory="img")
+graph.render(filename="test",format='png',directory="img")
