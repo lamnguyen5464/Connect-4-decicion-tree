4c6096e389c4e759cd1b1aa331e50fb2a485ba00
lamnguyen5464 <lamnguyen5464@gmail.com>
2021-12-12 14:45:00 +0700

    core: init preprocess

---
+++ pre-process/preprocess.py
@@ -0,0 +1,19 @@
+def show_row(input=''):
+    input = 'b,b,b,b,b,b,b,b,b,b,b,b,o,b,b,b,b,b,x,o,x,o,x,b,x,o,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,b,draw'
+
+    cells = input.split(',')
+
+    result = []
+
+    for i in range(0, 6):
+        row = []
+        for j in range(0, 7):
+            index = 6*(j+1) - i - 1
+            tick = not(cells[index] == 'b') and cells[index] or '_'
+            row.append(tick)
+        print(row)
+
+    print(cells[len(cells)-1])
+
+
+show_row()
